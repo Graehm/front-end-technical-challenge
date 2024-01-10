8ff259d7f94d80346d2d2e22d2a6873f7358cb51
Graehm <gpatrellfazio@gmail.com>
2024-01-10 16:08:57 -0600

    typo error resolved

--- src/views/Inventory/Inventory.tsx
+++ src/views/Inventory/Inventory.tsx
@@ -1,11 +1,10 @@
-import ModelList from "../../components/ModelList";
+import ModelList from "../../components/ModelList"
 
 const Inventory = () => {
   return (
     <div>
       <h1>Hello Inventory :)</h1>
-      
-      <ModelList /> {/* Ensure that the component name is ModelList, not Modelist */}
+      <ModelList />
     </div>
   );
 };
