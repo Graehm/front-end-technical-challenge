8bcac6291f9480ac1afdc116aab51b9d8530dd07
Graehm <gpatrellfazio@gmail.com>
2024-01-10 15:38:36 -0600

    new model card component integration

--- src/views/Inventory/Inventory.tsx
+++ src/views/Inventory/Inventory.tsx
@@ -1,7 +1,10 @@
+// import ModelCard from "../../components/ModelCard"
+
 const Inventory = () => {
   return (
     <div>
       <h1>Hello Inventory :)</h1>
+      {/* <ModelCard /> */}
     </div>
   );
 };
