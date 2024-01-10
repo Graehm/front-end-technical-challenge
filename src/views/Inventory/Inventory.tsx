1a902452df611d5c4d0ab3144a90d77b374eef90
Graehm <gpatrellfazio@gmail.com>
2024-01-10 15:47:33 -0600

    boiler plate stuff set up

--- src/views/Inventory/Inventory.tsx
+++ src/views/Inventory/Inventory.tsx
@@ -1,10 +1,10 @@
-// import ModelCard from "../../components/ModelCard"
+import ModelCard from "../../components/ModelCard"
 
 const Inventory = () => {
   return (
     <div>
       <h1>Hello Inventory :)</h1>
-      {/* <ModelCard /> */}
+      <ModelCard />
     </div>
   );
 };
