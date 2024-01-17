249fd722207a2674aec5fa6365a44082cbb6ac14
Graehm <gpatrellfazio@gmail.com>
2024-01-16 20:28:47 -0600

    nivo analysis set up

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import "./index.css";
 import Home from "./views/Home/Home.tsx";
 import Inventory from "./views/Inventory/Inventory.tsx";
+import Analysis from "./views/Analysis/Analysis.tsx";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
         path: "/inventory",
         element: <Inventory />,
       },
+      {
+        path: "/Analysis",
+        element: <Analysis />
+      }
     ],
   },
 ]);
--- src/views/Analysis/Analysis.tsx
+++ src/views/Analysis/Analysis.tsx
@@ -1,1 +1,6 @@
 // unsure if I need an analysis view file or just an analysis component
+
+const AnalysisPage = () => {
+  };
+  
+  export default AnalysisPage;
