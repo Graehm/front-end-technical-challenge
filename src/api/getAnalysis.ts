4fbfdc68ad5d5f8cdb3bc1f609a0db527c73518c
Graehm <gpatrellfazio@gmail.com>
2024-01-17 22:20:51 -0600

    i feel i went too far and might need to revert back to og getAnalysis function so coded out but added back in for reference

--- src/api/getAnalysis.ts
+++ src/api/getAnalysis.ts
@@ -14,12 +14,11 @@ export const getAnalysis = async (modelName: string) => {
     return { data, loading };
   }
 
-  // Filter out objects with numeric values
   data = analysisResponse[0]
     .filter((item) => typeof item.value !== 'object')
     .map((item) => ({
       origin: item.origin,
-      value: item.value as string[], // Ensure TypeScript recognizes the type
+      value: item.value as string[], 
       insight_name: item.insight_name,
       name: item.name,
     }));
@@ -28,3 +27,29 @@ export const getAnalysis = async (modelName: string) => {
 
   return { data, loading };
 };
+
+
+// import { analysisResponse, modelsResponse } from "./mockedData";
+// import { delay } from "./utils";
+
+// const modelNames = modelsResponse.map((model) => model.model_name);
+
+// export const getAnalysis = async (modelName: string) => {
+//   let loading = true;
+//   let data = [];
+
+//   await delay(1000);
+
+//   console.log(modelNames, modelName, modelNames.includes(modelName));
+//   if (!modelNames.includes(modelName)) {
+//     data = [null];
+//     loading = false;
+
+//     return { data, loading };
+//   }
+
+//   data = analysisResponse;
+//   loading = false;
+
+//   return { data, loading };
+// };
