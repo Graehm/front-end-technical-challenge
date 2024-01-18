f8977bcd7c326125b204e61b88b05cad35c1bc13
Graehm <gpatrellfazio@gmail.com>
2024-01-17 21:46:18 -0600

    syntax

--- src/views/Analysis/Analysis.tsx
+++ src/views/Analysis/Analysis.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getAnalysis } from '../../api/getAnalysis';
 import { ResponsiveBar } from '@nivo/bar';
