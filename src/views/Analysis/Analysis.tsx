a668e335c21ca2cda3267f8bfa112d4f36c954b7
Graehm <gpatrellfazio@gmail.com>
2024-01-17 21:37:54 -0600

    analysis views file

--- src/views/Analysis/Analysis.tsx
+++ src/views/Analysis/Analysis.tsx
@@ -1,6 +1,32 @@
-// unsure if I need an analysis view file or just an analysis component
+import { useParams } from 'react-router-dom';
+import { getAnalysis } from '../../api/getAnalysis';
+import { ResponsiveBar } from '@nivo/bar';
 
 const AnalysisPage = () => {
-  };
-  
-  export default AnalysisPage;
+  const { MODEL_NAME } = useParams();
+  const { data, loading } = getAnalysis(MODEL_NAME);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{MODEL_NAME}</h1>
+      <div className="chart-card">
+        <ResponsiveBar
+          data={data}
+          keys={['percentage']}
+          indexBy="origin"
+          layout="horizontal"
+          axisBottom={{
+            format: (value) => `${value}%`,
+          }}
+          enableGridX={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AnalysisPage;
