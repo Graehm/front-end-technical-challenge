283907f1d0369928165d6b18ed4a3c223210134c
Graehm <gpatrellfazio@gmail.com>
2024-02-02 21:42:10 -0600

    setting up next steps in analysis

--- src/views/Analysis/Analysis.tsx
+++ src/views/Analysis/Analysis.tsx
@@ -1,7 +1,7 @@
 // 1. what value should i pass through to the keys{[]} 
 // in ResponsiveBar2 explanation of how to transform the data to BarDatum-it wants item and index but our data doesnt have that sort of key:value pair 
 // 3. where to map over analysisresponse? leave getAnalysis alone and add functionality to analysis or do it in getAnalysis
-
+// 4. look at mockedData and Nivo. display the barchart like Wdiths and heuights to be hamburgers etc and then each has a whole value and a specific modal
 
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
