7f10ff01b27dda01964c661e969cc8d537753e9b
Graehm <gpatrellfazio@gmail.com>
2024-02-23 19:27:41 -0600

    trying to get individial bar chart to render based on modelName

--- src/api/getAnalysis.ts
+++ src/api/getAnalysis.ts
@@ -17,36 +17,11 @@ export const getAnalysis = async (modelName: string) => {
     return { data, loading };
   }
 
-  data = analysisResponse;
+  // data = analysisResponse;
+  // data = analysisResponse.map((entry) => {
+  //   entry.filter((item ) => item.name.startsWith(`${modelName}_`))
+  // })
   loading = false;
 
   return { data, loading };
 };
-
-
-
-// ---------- OG FILE ------------ //
-// import { analysisResponse, modelsResponse } from "./mockedData";
-// import { delay } from "./utils";
-
-// const modelNames = modelsResponse.map((model) => model.model_name);
-
-// export const getAnalysis = async (modelName: string) => {
-//   let loading = true;
-//   let data = [];
-
-//   await delay(1000);
-
-//   console.log(modelNames, modelName, modelNames.includes(modelName));
-//   if (!modelNames.includes(modelName)) {
-//     data = [null];
-//     loading = false;
-
-//     return { data, loading };
-//   }
-
-//   data = analysisResponse;
-//   loading = false;
-
-//   return { data, loading };
-// };
